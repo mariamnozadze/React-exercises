22fb72c5ed04d843e952a721a47b19614f341a2e
mariamnozadze <mariamnozadze97@yahoo.com>
2022-04-26 20:34:29 +0400

    upload useState & useEffect exercises

--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -1,9 +1,28 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 // by default runs after every re-render
 // cleanup function
 // second parameter
 const UseEffectBasics = () => {
-  return <h2>useEffect Basics</h2>;
+  const [value, setValue] = useState(0);
+
+  useEffect(() => {
+    console.log("call useEffect");
+
+    if (value >= 1) {
+      document.title = `New messages(${value})`;
+    }
+  }, [value]);
+
+  console.log("render component");
+
+  return (
+    <div>
+      <h1>{value}</h1>
+      <button className="btn" onClick={() => setValue(value + 1)}>
+        Click me
+      </button>
+    </div>
+  );
 };
 
 export default UseEffectBasics;
--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -1,12 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 // short-circuit evaluation
 // ternary operator
 
 const ShortCircuit = () => {
-  // const firstValue = text || 'hello world';
-  // const secondValue = text && 'hello world';
+  const [text, setText] = useState("");
+  const [isError, setIsError] = useState(false);
+  //  const firstValue = text || 'hello world';
+  //  const secondValue = text && 'hello';
 
-  return <h2>short circuit</h2>;
+  return (
+    <div>
+      {/*<h1>{firstValue}</h1>
+    <h1>value : {secondValue}</h1>*/}
+      <h1>{text || "john doe"}</h1>
+      <button className="btn" onClick={() => setIsError(!isError)}>
+        toggle error
+      </button>
+      {isError && <h1>Error...</h1>}
+      {isError ? (
+        <p>there is an error...</p>
+      ) : (
+        <div>
+          <h2>there is no error</h2>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default ShortCircuit;
