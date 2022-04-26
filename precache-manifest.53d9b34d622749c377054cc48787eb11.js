8183e644909a6c6826e61887a59ae37b57f543d3
mariamnozadze <mariamnozadze97@yahoo.com>
2022-04-26 20:32:59 +0400

    Updates

---
+++ precache-manifest.53d9b34d622749c377054cc48787eb11.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7c7bb9a68996fbb4e1422c0af504d5a",
+    "url": "/react-exercises/index.html"
+  },
+  {
+    "revision": "7928ec3e207bef88d1aa",
+    "url": "/react-exercises/static/css/main.07b738c9.chunk.css"
+  },
+  {
+    "revision": "7cc2373cd66ce3ee8ba3",
+    "url": "/react-exercises/static/js/2.7475bf84.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-exercises/static/js/2.7475bf84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7928ec3e207bef88d1aa",
+    "url": "/react-exercises/static/js/main.607d216a.chunk.js"
+  },
+  {
+    "revision": "a1c211eeb204fc0f9eec",
+    "url": "/react-exercises/static/js/runtime-main.2e7ddea4.js"
+  }
+]);
---
+++ static/js/main.607d216a.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpreact-exercises"]=this["webpackJsonpreact-exercises"]||[]).push([[0],{3:function(e,t,n){e.exports=n(9)},8:function(e,t,n){},9:function(e,t,n){"use strict";n.r(t);var r=n(0),a=n.n(r),c=n(2),l=n.n(c),s=(n(8),function(){return a.a.createElement("h2",null,"useRef")});var u=function(){return a.a.createElement("div",{className:"container"},a.a.createElement(s,null))};l.a.render(a.a.createElement(a.a.StrictMode,null,a.a.createElement(u,null)),document.getElementById("root"))}},[[3,1,2]]]);
+//# sourceMappingURL=main.607d216a.chunk.js.map
